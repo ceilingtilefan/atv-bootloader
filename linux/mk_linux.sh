0be31103494d6f93035a63eeb7b6beafe48755d2
bigfloppa234 <104149053+ceilingtilefan@users.noreply.github.com>
2024-11-30 09:21:01 -0600

    Update kernel version

--- linux/mk_linux.sh
+++ linux/mk_linux.sh
@@ -3,15 +3,15 @@
 SCRIPT_DIR=$(dirname `readlink -f "$0"`)
 cd "${SCRIPT_DIR}"
 
-VERSION="2.6.39"
+VERSION="6.6.63"
 CONFIG="linux-${VERSION}.config"
 
-if [ ! -f linux-${VERSION}.tar.bz2 ] ; then
-	wget http://www.kernel.org/pub/linux/kernel/v2.6/linux-${VERSION}.tar.bz2
+if [ ! -f linux-${VERSION}.tar.gz ] ; then
+	wget http://www.kernel.org/pub/linux/kernel/v2.6/linux-${VERSION}.tar.gz
 fi
 #
 #
-tar -jxf linux-${VERSION}.tar.bz2
+tar -jxf linux-${VERSION}.tar.gz
 
 if [ -f "${CONFIG}" ] ; then
 	cp "${CONFIG}"  linux-${VERSION}/.config
